8bdff258736f02c65755ac22a76bb4b40ec735e3
MatheusSilva31 <matheus.santos-silva@hotmail.com>
2022-09-07 16:01:32 -0300

    gravando dados na tabela questionario e alterando flag user_ativo

--- backend/src/controllers/formularioPedagogicoController.js
+++ backend/src/controllers/formularioPedagogicoController.js
@@ -2,53 +2,40 @@ import db from '../config/dbConnect.js';
 
 class formularioPedagogicoController {
 
-    static GravarFormulario = async (req, res) => {
-
-        // const arr = [user_id_aprendiz,id_pergunta,resposta,peso,tipo_alternativa]
-
-        // const { user_id_aprendiz,id_pergunta,resposta,peso,tipo_alternativa } = req.body;
-        // const arr = [req.body.user_id_aprendiz,req.body.id_pergunta,req.body.resposta,req.body.peso,req.body.tipo_alternativa]
-
-        // let my_array = new array('x', 'y', 'z')
-        // let arr2 = [ {"user_id_aprendiz":3,"id_pergunta":10,"resposta":2,"peso":15," tipo_alternativa":"video"},
-        // {"user_id_aprendiz":4,"id_pergunta":10,"resposta":2,"peso":16," tipo_alternativa":"video"},
-        // {"user_id_aprendiz":4,"id_pergunta":10,"resposta":2,"peso":17," tipo_alternativa":"video"}]
-
-        // let arr2 = req.body
-        //    for(let i = 0;i<arr2.length;i++){
-        //      db.any(
-        //         `INSERT INTO shae_db.questionario_pedagogico
-        //         (user_id_aprendiz, id_pergunta, resposta, peso, tipo_alternativa)
-        //         VALUES($1:list)`,[arr2[i]]
-        //     ).then((result)=>{
-        //         console.log(result)
-
-        //     }).catch((error)=>{
-        //         res.status(201).send({
-        //             message: "erro",
-        //         });
-        //         console.log("dentro do catch ",error)
-
-        //     })
-        //    }
-
-  
+    static GravarFormulario = async (req, res) => {  
     let arr2 = req.body
+    const id_user = arr2[0].user_id_aprendiz;
 
     try{
         for(let i = 0;i<arr2.length;i++){
         let sql = await db.any(`INSERT INTO shae_db.questionario_pedagogico(user_id_aprendiz, id_pergunta, resposta, peso, tipo_alternativa)VALUES($1:list)`, [arr2[i]]);
         }
-        res.status(200).json({ 
-            message:"dados gravados com sucesso"
+       
+        let sql2 = await db.any(`select * from shae_db.usuarioAlteraStatus($1)`,[id_user])
+        .then((resp)=>{
+            console.log(resp)
+            console.log()
+            res.status(200).json({ 
+                message:"dados gravados com sucesso e flag alterada com sucesso"
+            })
+
+        }).catch((erro)=>{
+            console.log(erro)
+            res.status(400).json({
+            message:"houve um erro com sua solicitação"
         })
+        })
+
+       
     }catch(err){
+        console.log(err)
         res.status(400).json({
             message:"houve um erro com sua solicitação",messegaBD:err
         })
     }
 
     }
+    
 
 }
 
